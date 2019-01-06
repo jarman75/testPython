e54854ac4d749332a9237c22f8b5d02e1d1669c7
Armando Martí Carretero <armando.marti.carretero@gmail.com>
2019-01-06 12:11:37 +0100

    inicial commit

---
+++ basic/modulo.py
@@ -0,0 +1,7 @@
+class complejo:
+    def __init__(self, partereal, parteimaginaria):
+        self.r = partereal
+        self.i = parteimaginaria
+       
+    def suma(self):
+            return self.r + self.i
---
+++ basic/test.py
@@ -0,0 +1,6 @@
+import modulo
+
+print("Hello world")
+x = modulo.complejo(2,4)
+print(x.r, x.i)
+print( x.suma() )
